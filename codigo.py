ddf02f27e7958e96f5965ff68f57dea70bec879f
Guillermo02 <guigokuznietz@gmail.com>
2021-05-24 17:43:27 -0300

    atual

--- codigo.py
+++ codigo.py
@@ -14,10 +14,11 @@ pygame.display.set_caption('Pygame')
 #assets jogador
 JOG_WIDTH = 90
 JOG_HEIGHT = 70
-jog_img = pygame.image.load('Pygame-beto-raymond-guiliermo/Sprites/jogador1.png').convert_alpha()
+jog_img = pygame.image.load('Sprites/jogador1.png').convert_alpha()
+jog_img = pygame.image.load('Sprites/jogador1.png').convert_alpha()
 jog_img = pygame.transform.scale(jog_img, (JOG_WIDTH, JOG_HEIGHT))
 #assets inimigo
-inim_img = pygame.image.load('Pygame-beto-raymond-guiliermo/Sprites/bacteria1.png').convert_alpha()
+inim_img = pygame.image.load('Sprites/bacteria1.png').convert_alpha()
 inim_img = pygame.transform.scale(inim_img, (70, 70))
 #####PONTOS######
 #assets gemas
@@ -27,16 +28,16 @@ inim_img = pygame.transform.scale(inim_img, (70, 70))
 #t_gemas = [gemab_img, gemay_img, gemag_img]
 #################
 #assets chão
-chao_img = pygame.image.load("Pygame-beto-raymond-guiliermo/Sprites/plataforma.png").convert_alpha()
+chao_img = pygame.image.load("Sprites/plataforma.png").convert_alpha()
 chao_img = pygame.transform.scale(chao_img, (710, 200))
 #assets background
-bg = pygame.image.load("Pygame-beto-raymond-guiliermo/Sprites/hospital2.png.jpg")
+bg = pygame.image.load("Sprites/hospital2.png.jpg")
 #background = pygame.image.load('sprite do fundo').convert()
 background = pygame.transform.scale(bg, (700, 620))
 background_rect = background.get_rect()
 ###FONTE DE TEXTO QUE O ANDREW TINHA DISPONIBILIZADO###
 #assets fonte de texto
-score_font = pygame.font.Font('Pygame-beto-raymond-guiliermo/font/PressStart2P.ttf', 28)
+score_font = pygame.font.Font('font/PressStart2P.ttf', 28)
 
 
 ########EXTRAS#########
