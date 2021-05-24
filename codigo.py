3bc3522817c7d1f541d76bae11bb17ac35585291
Raymond <raydiwan@gmail.com>
2021-05-24 16:25:43 -0300

    tentativa

--- codigo.py
+++ codigo.py
@@ -6,7 +6,7 @@ pygame.init()
 WIDTH = 700
 HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('COVID SLAYER')
+pygame.display.set_caption('Nome não definido ainda')
 
 
 # ----- Inicia assets
