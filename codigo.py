e7f09176964273048a9c20bb7f75039d41a8978b
Raymond <raydiwan@gmail.com>
2021-05-31 16:15:35 -0300

    changes

--- codigo.py
+++ codigo.py
@@ -3,10 +3,10 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 700
+WIDTH = 1000
 HEIGHT = 550
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Covid Slay')
+pygame.display.set_caption('Pygame')
 
 
 # ----- Inicia assets
@@ -14,6 +14,8 @@ pygame.display.set_caption('Covid Slay')
 #assets jogador
 JOG_WIDTH = 90
 JOG_HEIGHT = 70
+
+jog_img = pygame.image.load('Sprites/gunman.png').convert_alpha()
 jog_img = pygame.transform.scale(jog_img, (JOG_WIDTH, JOG_HEIGHT))
 #assets inimigo
 inim_img = pygame.image.load('Sprites/bacteria1.png').convert_alpha()
@@ -29,10 +31,11 @@ inim_img = pygame.transform.scale(inim_img, (70, 70))
 chao_img = pygame.image.load("Sprites/plataforma.png").convert_alpha()
 chao_img = pygame.transform.scale(chao_img, (710, 200))
 #assets background
-bg = pygame.image.load("Sprites/hospital2.png.jpg")
-#background = pygame.image.load('sprite do fundo').convert()
-background = pygame.transform.scale(bg, (700, 620))
+bg = pygame.image.load("Sprites/hospital2.png.jpg").convert_alpha()
+background = pygame.transform.scale(bg, (1000, 620))
 background_rect = background.get_rect()
+#assets do tiro
+bullet_img = pygame.image.load('Sprites/laserRed16.png').convert_alpha()
 ###FONTE DE TEXTO QUE O ANDREW TINHA DISPONIBILIZADO###
 #assets fonte de texto
 score_font = pygame.font.Font('font/PressStart2P.ttf', 28)
@@ -48,7 +51,7 @@ PONTOS = 0
 VIDAS = 3
 #######################
 
-#Classe do jogador
+#Cl asse do jogador
 class jogador(pygame.sprite.Sprite):
     def __init__(self, jog_img, VIDAS):
         pygame.sprite.Sprite.__init__(self)
@@ -63,6 +66,9 @@ class jogador(pygame.sprite.Sprite):
         self.speedx = 0
         self.speedy = 0
         self.lifes = VIDAS
+        self.sprites = all_sprites
+        self.bullets = all_bullets
+        self.tiro  = bullet_img
     
     def update(self):
 
@@ -72,12 +78,6 @@ class jogador(pygame.sprite.Sprite):
         self.rect.y += self.speedy
         #Atualiza a posição x
         self.rect.x += self.speedx
-
-        #Se jogador colidiu com algum inimigo
-        #collisions = pygame.sprite.spritecollide(self, inimigo, False)
-        #Perde uma vida
-        #for collision in collisions:
-            #self.lifes -= 1
         
         #Corrige a posição para não sair da janela
         if self.rect.left < 0:
@@ -96,60 +96,12 @@ class jogador(pygame.sprite.Sprite):
             self.speedy -= 50
             self.state = JUMPING    
 
-
+    #Função do tiro
     def shoot(self):
         # A nova bala vai ser criada logo acima e no centro horizontal da nave
-        new_bullet = Bullet(self.kisspng-syringe-injection-vector-syringes.jpg, self.rect.top, self.rect.centerx)
-        self.all_sprites.add(new_bullet)
-        self.all_bullets.add(new_bullet)
-
-class Meteor(pygame.sprite.Sprite):
-    def __init__(self, img):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = img
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, WIDTH)
-        self.rect.y = random.randint(-100, HEIGHT)
-        self.speedx = random.randint(-3, 3)
-        self.speedy = random.randint(2, 9)
-
-    def update(self):
-        # Atualizando a posição do meteoro
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        # Se o meteoro passar do final da tela, volta para cima e sorteia
-        # novas posições e velocidades
-        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
-            self.rect.x = random.randint(0, WIDTHR_WIDTH)
-            self.rect.y = random.randint(-100, HEIGHT)
-            self.speedx = random.randint(-3, 3)
-            self.speedy = random.randint(2, 9)
-
-# Classe Bullet que representa os tiros
-class Bullet(pygame.sprite.Sprite):
-    # Construtor da classe.
-    def __init__(self, img, bottom, centerx):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = img
-        self.rect = self.image.get_rect()
-
-        # Coloca no lugar inicial definido em x, y do constutor
-        self.rect.centerx = centerx
-        self.rect.bottom = bottom
-        self.speedy = -10  # Velocidade fixa para cima
-
-    def update(self):
-        # A bala só se move no eixo y
-        self.rect.y += self.speedy
-
-        # Se o tiro passar do inicio da tela, morre.
-        if self.rect.bottom < 0:
-            self.kill()
-
+        new_bullet = Bullet(self.tiro, self.rect.centery, self.rect.centerx+40)
+        self.bullets.add(new_bullet)
+        self.sprites.add(new_bullet)
 
 #Classe do inimigo
 class inimigo(pygame.sprite.Sprite):
@@ -159,8 +111,8 @@ class inimigo(pygame.sprite.Sprite):
         # Define estado atual
         self.image = inim_img
         self.rect = self.image.get_rect()
-        self.rect.centerx = 200
-        self.rect.centery = 200
+        self.rect.centerx = WIDTH - 20
+        self.rect.centery = HEIGHT - 40
         self.speedx = 0
         self.speedy = 0
     
@@ -170,16 +122,28 @@ class inimigo(pygame.sprite.Sprite):
         if self.rect.left > WIDTH:
             self.rect.right = -100   
 
-class Plataforma (pygame.sprite.Sprite):
-    def __init__(self,chao_img):
-        
+#Classe do tiro
+class Bullet(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self, assets, bottom, centerx):
+        # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
-        
-        self.image = chao_img
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH/2
-        self.rect.y = HEIGHT-10
 
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.centerx = centerx
+        self.rect.bottom = bottom
+        self.speedx = 20 #Velocidade fixa pro lado
+
+    def update(self):
+        # A bala só se move no eixo y
+        self.rect.x += self.speedx
+
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.left > WIDTH:
+            self.kill()
 
 game = True
 #Cria um relógio que conta o tempo em jogo
@@ -189,17 +153,14 @@ FPS = 20
 #############COLISõES#############
 #Cria grupos com as sprites e collides
 all_sprites = pygame.sprite.Group()
+all_bullets = pygame.sprite.Group()
 collide_enemy = pygame.sprite.Group()
-collide_gema = pygame.sprite.Group()
-collide_meteoros = pygame.sprite.Group()
 # Criando o jogador, inimigo e gemas
 player = jogador(jog_img, VIDAS)
 enemy = inimigo(inim_img)
-chao = Plataforma(chao_img)
 #Adicionando sprites em uma variável global
 all_sprites.add(player)
 all_sprites.add(enemy)
-all_sprites.add(chao)
 collide_enemy.add(enemy)
 ###################################
 
@@ -220,12 +181,29 @@ while game:
                     player.speedx -= 8
                 if event.key == pygame.K_RIGHT:
                     player.speedx += 8
-                elif event.key == pygame.K_UP:
+                if event.key == pygame.K_UP:
                     player.jump()
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
             # Verifica se soltou alguma tecla.
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     player.speedx = 0
+
+        hits = pygame.sprite.spritecollide(player, collide_enemy, True, pygame.sprite.collide_mask)
+        hit2 = pygame.sprite.groupcollide(all_bullets, collide_enemy, True, True)
+
+        if len(hits) > 0:
+            VIDAS -= 1
+            enemy.rect.x = -200
+            all_sprites.add(enemy)
+            collide_enemy.add(enemy)
+        if len(hit2) > 0:
+            enemy.rect.x = -200
+            all_sprites.add(enemy)
+            collide_enemy.add(enemy)
+
+        
 
         all_sprites.update()
         collide_enemy.update()
@@ -247,6 +225,15 @@ while game:
         text_rect = text_surface.get_rect()
         text_rect.bottomleft = (10, HEIGHT - 10)
         window.blit(text_surface, text_rect)
+
+    if VIDAS<1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game = False
+            
 
     pygame.display.update()  # Mostra o novo frame para o jogador
 # ===== Finalização =====
